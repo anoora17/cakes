63d0399a2850ce4257738a7cab52a93968c9f2b3
Your Name <anoor@witweel.com>
2017-09-01 01:10:00 -0400

    progress1

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -13,4 +13,16 @@ var router = express.Router();
 	router.get('/order', function (req, res) {
 		res.render('dashboard')
 	});
+
+	router.post('/register', function (req, res) {
+		
+		var firstname = req.body.firstname;
+		var lastname  = req.body.lastname;
+		var email     = req.body.email;
+		var password  = req.body.password;
+		var city      = req.body.city;
+		var zipcode   = req.body.zipcode
+		console.log(firstname)
+	});
+
 module.exports = router;
